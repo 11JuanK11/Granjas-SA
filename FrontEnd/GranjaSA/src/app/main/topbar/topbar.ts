2b4d6eb41e6702ff994d0e1264d34c407542175f
JuanK <camiloalzatebedoya15@gmail.com>
2025-08-31 13:45:22 -0500

    Actualizar estructura topbar componente

--- FrontEnd/GranjaSA/src/app/main/topbar/topbar.ts
+++ FrontEnd/GranjaSA/src/app/main/topbar/topbar.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-topbar',
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './topbar.html',
   styleUrl: './topbar.scss'
 })
 export class Topbar {
-
 }
