97c7e5db090744dd0dfa0577be7b7a80f968c551
JuanK <camiloalzatebedoya15@gmail.com>
2025-08-31 08:40:39 -0500

    Uso de componente main content

--- FrontEnd/GranjaSA/src/app/app.ts
+++ FrontEnd/GranjaSA/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Topbar } from './main/topbar/topbar';
 import { Sidebar } from './main/sidebar/sidebar';
+import { MainContent } from "./main/main-content/main-content";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Topbar, Sidebar],
+  imports: [RouterOutlet, Topbar, Sidebar, MainContent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
