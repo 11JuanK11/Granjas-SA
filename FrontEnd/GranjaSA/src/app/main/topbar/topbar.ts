9d4e3619eda346f57a45cdb9c9eb23534e49ff33
JuanK <camiloalzatebedoya15@gmail.com>
2025-09-01 11:00:59 -0500

    Agregar nuevos elementos a top bar component

--- FrontEnd/GranjaSA/src/app/main/topbar/topbar.ts
+++ FrontEnd/GranjaSA/src/app/main/topbar/topbar.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-topbar',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatTooltipModule],
   templateUrl: './topbar.html',
   styleUrl: './topbar.scss'
 })
