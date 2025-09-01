702adf6d58714840d5e2e86b76b6638edaf6fcca
JuanK <camiloalzatebedoya15@gmail.com>
2025-09-01 08:23:56 -0500

    Cambios de la sidebar
    Mejoras en la estructura del componente

--- FrontEnd/GranjaSA/src/app/main/sidebar/sidebar.ts
+++ FrontEnd/GranjaSA/src/app/main/sidebar/sidebar.ts
@@ -1,11 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import {MatListModule} from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule, RouterOutlet],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss'
 })
 export class Sidebar {
-    @Input() isOpen: boolean = false;
+
 }
