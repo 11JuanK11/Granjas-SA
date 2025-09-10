c50e5ca81316020dc37af83a2b9bb1097a50931c
JuanK <camiloalzatebedoya15@gmail.com>
2025-09-09 21:31:15 -0500

    implementacion de servicios

--- FrontEnd/GranjaSA/src/app/main/content/porcinos/porcinos.ts
+++ FrontEnd/GranjaSA/src/app/main/content/porcinos/porcinos.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, inject, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import {MatSort, MatSortModule} from '@angular/material/sort';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
@@ -8,27 +8,47 @@ import {Porcino} from '../../Domain/Porcino';
 import {Cliente} from '../../Domain/Cliente';
 import {RazaPorcino} from '../../Domain/RazaPorcino';
 import {Alimentacion} from '../../Domain/Alimentacion';
+import { ServicioPorcino } from 'app/main/Services/ServicioPorcino/servicio-porcino';
+import { catchError, tap } from 'rxjs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 
 
 @Component({
   selector: 'app-porcinos',
-  imports: [MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule],
+  imports: [MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule, MatIconModule, MatButtonModule, MatTooltipModule],
   templateUrl: './porcinos.html',
   styleUrl: './porcinos.scss'
 })
-export class Porcinos implements AfterViewInit {
-    displayedColumns: string[] = ['Identificador', 'Raza', 'Edad', 'Peso', 'Alimentacion', 'Cliente'];
+export class Porcinos implements AfterViewInit{
+  displayedColumns: string[] = ['Identificador', 'Raza', 'Edad', 'Peso', 'Alimentacion', 'Cliente', 'Acciones'];
   dataSource: MatTableDataSource<Porcino>;
+  servicioPorcino = inject(ServicioPorcino);
+  porcinos: Porcino[] = [];
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
   constructor() {
-    // Create 100 users
-    const porcinos = this.porcinos;
-
-    this.dataSource = new MatTableDataSource(porcinos);
+    this.servicioPorcino.getAll().subscribe({
+      next: (porcinos) => {
+        console.log("Lista de porcinos:", porcinos);
+        this.porcinos = porcinos.map(p => ({
+          ...p,
+            cliente: p.cliente ?? {
+            cedula: 0,
+            nombres: "Por definir",
+            apellidos: "",
+            direccion: "",
+            telefono: ""
+          }
+      }));
+      },
+      error: (err) => console.error("Error:", err)
+    });
+    this.dataSource = new MatTableDataSource(this.porcinos);  
   }
 
   ngAfterViewInit() {
@@ -57,28 +77,14 @@ export class Porcinos implements AfterViewInit {
     }
   }
 
-  porcinos: Porcino[] = [
-    new Porcino(1, RazaPorcino.YORK, 6, 80, new Alimentacion(1, 'Dieta de engorde', '2kg/día'), undefined),
-    new Porcino(2, RazaPorcino.HAMP, 5, 75, new Alimentacion(2, 'Dieta de crecimiento', '1.5kg/día'), undefined),
-    new Porcino(3, RazaPorcino.DUROC, 8, 90, new Alimentacion(3, 'Dieta de mantenimiento', '1.8kg/día'), undefined),
-    new Porcino(4, RazaPorcino.YORK, 7, 85, new Alimentacion(1, 'Dieta de engorde', '2.2kg/día'), undefined),
-    new Porcino(5, RazaPorcino.HAMP, 4, 70, new Alimentacion(2, 'Dieta de crecimiento', '1.4kg/día'), undefined),
-    new Porcino(6, RazaPorcino.DUROC, 9, 95, new Alimentacion(3, 'Dieta de mantenimiento', '2kg/día'), undefined),
-    new Porcino(7, RazaPorcino.YORK, 5, 78, new Alimentacion(1, 'Dieta de engorde', '1.9kg/día'), undefined),
-    new Porcino(8, RazaPorcino.HAMP, 6, 82, new Alimentacion(2, 'Dieta de crecimiento', '1.6kg/día'), undefined),
-    new Porcino(9, RazaPorcino.DUROC, 7, 88, new Alimentacion(3, 'Dieta de mantenimiento', '1.7kg/día'), undefined),
-    new Porcino(10, RazaPorcino.YORK, 8, 92, new Alimentacion(1, 'Dieta de engorde', '2.1kg/día'), undefined),
-    new Porcino(11, RazaPorcino.HAMP, 5, 76, new Alimentacion(2, 'Dieta de crecimiento', '1.5kg/día'), undefined),
-    new Porcino(12, RazaPorcino.DUROC, 6, 83, new Alimentacion(3, 'Dieta de mantenimiento', '1.6kg/día'), undefined),
-    new Porcino(13, RazaPorcino.YORK, 9, 94, new Alimentacion(1, 'Dieta de engorde', '2.3kg/día'), undefined),
-    new Porcino(14, RazaPorcino.HAMP, 7, 86, new Alimentacion(2, 'Dieta de crecimiento', '1.7kg/día'), undefined),
-    new Porcino(15, RazaPorcino.DUROC, 4, 68, new Alimentacion(3, 'Dieta de mantenimiento', '1.4kg/día'), undefined),
-    new Porcino(16, RazaPorcino.YORK, 6, 81, new Alimentacion(1, 'Dieta de engorde', '2kg/día'), undefined),
-    new Porcino(17, RazaPorcino.HAMP, 8, 89, new Alimentacion(2, 'Dieta de crecimiento', '1.8kg/día'), undefined),
-    new Porcino(18, RazaPorcino.DUROC, 5, 74, new Alimentacion(3, 'Dieta de mantenimiento', '1.5kg/día'), undefined),
-    new Porcino(19, RazaPorcino.YORK, 7, 87, new Alimentacion(1, 'Dieta de engorde', '2.1kg/día'), undefined),
-    new Porcino(20, RazaPorcino.HAMP, 9, 93, new Alimentacion(2, 'Dieta de crecimiento', '1.9kg/día'), undefined)
-];
-  
-  
+editarPorcino(id: string) {
+  console.log("Editar porcino con ID:", id);
+  // Aquí podrías abrir un modal o redirigir a un formulario de edición
+}
+
+eliminarPorcino(id: string) {
+  console.log("Eliminar porcino con ID:", id);
+  // Aquí puedes llamar al servicio para eliminar el porcino
+}
+
 }
