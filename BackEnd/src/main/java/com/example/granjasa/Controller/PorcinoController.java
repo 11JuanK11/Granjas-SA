6be5ddb465aa313fc4e9d67971780161cf45a54d
SaraSanchez <sara_sanchez82221@elpoli.edu.co>
2025-09-11 18:33:21 -0500

    Clase controller para porcino

---
+++ BackEnd/src/main/java/com/example/granjasa/Controller/PorcinoController.java
@@ -0,0 +1,10 @@
+package com.example.granjasa.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/porcino/")
+public class PorcinoController {
+    
+}
